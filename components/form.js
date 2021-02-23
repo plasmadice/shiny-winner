496ff5d30339f7c722b9e295af10cb1fe6778990
plasmadice <phoenixquint@gmail.com>
2021-02-22 22:19:05 -0500

    Added hover transition to search bar

--- components/form.js
+++ components/form.js
@@ -2,8 +2,28 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+    width: '35ch',
+    '& :focus': {
+      transition: 'all 1s ease',
+      transform: 'scale(1.5)',
+      opacity: 0.7,
+    },
+
+    '& :hover': {
+      transition: 'all 1s ease',
+      transform: 'scale(1.5)',
+      opacity: 0.7,
+    },
+  },
+}));
 
 const Form = () => {
+  const classes = useStyles();
   const [value, setValue] = React.useState('');
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -12,16 +32,18 @@ const Form = () => {
   return (
     <form>
       <TextField
+        className={classes.root}
         id="input-with-icon-textfield"
         placeholder="Enter location..."
         value={value}
         onChange={handleChange}
         InputProps={{
           endAdornment: (
-            <InputAdornment position="start">
+            <InputAdornment position="end">
               <SearchIcon />
             </InputAdornment>
           ),
+          autoFocus: true,
         }}
       />
     </form>
